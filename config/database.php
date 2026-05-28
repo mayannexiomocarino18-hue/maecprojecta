4b94938d0f8150ddf06d3d8cdb7ddaffff8e7bd4
May Anne carino <mayannexiomocarino18@gmail.com>
2026-05-29 00:15:01 +0800

    Use explicit MySQL host settings in production

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL') ?: env('DATABASE_URL') ?: env('MYSQL_URL') ?: env('MYSQL_PUBLIC_URL'),
+            'url' => null,
             'host' => env('DB_HOST') ?: env('MYSQLHOST'),
             'port' => env('DB_PORT') ?: env('MYSQLPORT', '3306'),
             'database' => env('DB_DATABASE') ?: env('MYSQLDATABASE'),
